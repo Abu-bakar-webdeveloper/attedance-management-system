7f2044ddd7dc85ed132cd231712193cdc4635cdb
abubkar <abubakaraw8@gmail.com>
2024-06-28 09:36:39 +0500

    routes defined for user

--- backend/src/models/grade.model.js
+++ backend/src/models/grade.model.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 const GradeSchema = new mongoose.Schema({
   criteria: String,
   minAttendance: Number,
--- backend/src/models/leaverequest.model.js
+++ backend/src/models/leaverequest.model.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 const LeaveRequestSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   startDate: Date,
